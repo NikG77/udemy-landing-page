3e3b6e72f3774a94a6c3011444b05029b0a1fa61
Georg <georgiy.nikitin@gmail.com>
2020-03-05 19:48:42 +0300

    Корректировка отрицательного времени и двузначного отображения

--- js/timer.js
+++ js/timer.js
@@ -1,7 +1,7 @@
 'use strict';
 
 (function () {
-    let deadLine = '2020-03-15';
+    let DEADLINE = '2020-03-08';
 
     function getTimeRemaining(endTime) {
         let t = Date.parse(endTime) - Date.parse(new Date());
@@ -17,22 +17,36 @@
     }
 
     function setClock (id, endTime) {
+        
         let timer = document.getElementById(id);
         let hours = timer.querySelector('.hours');
         let minutes = timer.querySelector('.minutes');
         let seconds = timer.querySelector('.seconds');
-        let timeInterval = setInterval(updateClock, 1000);
+
+        if (Date.parse(endTime) <= Date.parse(new Date())) {
+            hours.textContent = '00';
+            minutes.textContent = '00';
+            seconds.textContent = '00';     
+        } else  {
+            let timeInterval = setInterval(updateClock, 1000);
+        }
 
         function updateClock () {
             let t = getTimeRemaining(endTime);
-            hours.textContent = t.hours;
-            minutes.textContent = t.minutes;
-            seconds.textContent = t.seconds;
+
+            function addZero(num){
+                if(num <= 9) {
+                    return '0' + num;
+                } else return num;
+            };
+            hours.textContent = addZero(t.hours);
+            minutes.textContent = addZero(t.minutes);
+            seconds.textContent = addZero(t.seconds);
             if (t.total <= 0) {
                 clearInterval(timeInterval);
             }
         }
     }
 
-    setClock('timer', deadLine);
+    setClock('timer', DEADLINE);
 })();
